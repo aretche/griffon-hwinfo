526a5f0f796391d84b17609735b61b842907f780
Gabriel Arellano <arellanog@cent.gov.ar>
2017-10-27 16:45:08 -0300

    Agrego atributos y métodos para mostrar la información del hardware

--- griffon-app/controllers/io/github/aretche/hwlist/HwInfoController.groovy
+++ griffon-app/controllers/io/github/aretche/hwlist/HwInfoController.groovy
@@ -5,6 +5,11 @@ import griffon.core.controller.ControllerAction
 import griffon.inject.MVCMember
 import griffon.metadata.ArtifactProviderFor
 import griffon.transform.Threading
+import oshi.SystemInfo
+import oshi.hardware.HardwareAbstractionLayer
+import oshi.hardware.NetworkIF
+import oshi.software.os.OperatingSystem
+
 import javax.annotation.Nonnull
 
 @ArtifactProviderFor(GriffonController)
@@ -15,7 +20,40 @@ class HwInfoController {
     @ControllerAction
     @Threading(Threading.Policy.INSIDE_UITHREAD_ASYNC)
     void refreshInfo() {
-        model.detail = new Date().format("yyyyMMdd-HH:mm:ss.SSS", TimeZone.getTimeZone('UTC'))
-        println model.detail
+        SystemInfo si = new SystemInfo()
+        OperatingSystem os = si.getOperatingSystem()
+        model.osName = os.family
+        model.osVersion = os.version
+
+        HardwareAbstractionLayer hal = si.getHardware()
+        model.motherMarca = hal.computerSystem.baseboard.manufacturer
+        model.motherModelo = hal.computerSystem.baseboard.model
+
+        model.cpuID = hal.processor.getProcessorID()
+        model.cpu = hal.processor.getIdentifier()
+
+        // Tomo la primera placa de red con tráfico
+        hal.getNetworkIFs().any { NetworkIF net ->
+            if(net.getBytesRecv() > 0){
+                setNetworkInfo(net.name)
+                true
+            } else {
+                return
+            }
+        }
+
+        model.detail = model.toString()
+    }
+
+    def setNetworkInfo(String ifName){
+        SystemInfo si = new SystemInfo()
+        HardwareAbstractionLayer hal = si.getHardware()
+        model.netIF = model.macAdress = ""
+        hal.getNetworkIFs().any { NetworkIF net ->
+            if(ifName == net.name){
+                model.netIF = net.name
+                model.macAdress = net.getMacaddr()
+            }
+        }
     }
 }
--- griffon-app/models/io/github/aretche/hwlist/HwInfoModel.groovy
+++ griffon-app/models/io/github/aretche/hwlist/HwInfoModel.groovy
@@ -7,10 +7,36 @@ import griffon.transform.FXObservable
 @ArtifactProviderFor(GriffonModel)
 class HwInfoModel {
 
-    @FXObservable
-    String osInfo = ""
-    @FXObservable
+    String osName = ""
+    String osVersion = ""
+    String motherMarca = ""
+    String motherModelo = ""
+    String cpu = ""
+    String cpuID = ""
+    String netIF = ""
     String macAdress = ""
     @FXObservable
     String detail = ""
+
+
+    String toString(){
+        String out = ""
+        if(osName)
+            out += "OS name: ${osName}\n"
+        if(osVersion)
+            out += "OS version: ${osVersion}\n"
+        if(motherMarca)
+            out += "Base board: ${motherMarca}\n"
+        if(motherModelo)
+            out += "Base board model: ${motherModelo}\n"
+        if(cpu)
+            out += "Processor: ${cpu}\n"
+        if(cpuID)
+            out += "CPU ID: ${cpuID}\n"
+        if(netIF)
+            out += "Network IF: ${netIF}\n"
+        if(macAdress)
+            out += "MAC Address: ${macAdress}\n"
+        out
+    }
 }
