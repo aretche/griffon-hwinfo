44925a1ad408f89731c97ba223fadbee621dd260
Gabriel Arellano <arellanog@cent.gov.ar>
2017-10-29 19:59:04 -0300

    Muevo todo el código dependiente de OSHI al HwInfoService

--- griffon-app/controllers/io/github/aretche/hwlist/HwInfoController.groovy
+++ griffon-app/controllers/io/github/aretche/hwlist/HwInfoController.groovy
@@ -5,90 +5,36 @@ import griffon.core.controller.ControllerAction
 import griffon.inject.MVCMember
 import griffon.metadata.ArtifactProviderFor
 import griffon.transform.Threading
-import oshi.SystemInfo
-import oshi.hardware.HardwareAbstractionLayer
-import oshi.hardware.NetworkIF
-import oshi.software.os.OperatingSystem
 
 import javax.annotation.Nonnull
+import javax.inject.Inject
 
 @ArtifactProviderFor(GriffonController)
 class HwInfoController {
     @MVCMember @Nonnull
     HwInfoModel model
 
+    @Inject
+    private HwInfoService hwInfoService
+
     @ControllerAction
     @Threading(Threading.Policy.INSIDE_UITHREAD_ASYNC)
     void refreshInfo() {
-        SystemInfo si = new SystemInfo()
-        OperatingSystem os = si.getOperatingSystem()
-        model.osName = os.family
-        model.osVersion = os.version
+        model.osName = hwInfoService.getOsName()
+        model.osVersion = hwInfoService.getOsVersion()
 
-        HardwareAbstractionLayer hal = si.getHardware()
-        model.motherMarca = hal.computerSystem.baseboard.manufacturer
-        model.motherModelo = hal.computerSystem.baseboard.model
+        model.motherMarca = hwInfoService.getbaseBoardMaker()
+        model.motherModelo = hwInfoService.getbaseBoardModel()
 
-        model.cpuID = hal.processor.getProcessorID()
-        model.cpu = hal.processor.getIdentifier()
+        model.cpuID = hwInfoService.getProcessorId()
+        model.cpu = hwInfoService.getProcessor()
 
-        String ifaceName = getPrimaryIfaceName()
-        if(ifaceName)
-            setNetworkInfo(ifaceName)
+        String ifaceName = hwInfoService.getPrimaryIfaceName()
+        if(ifaceName){
+            model.netIF = ifaceName
+            model.macAdress = hwInfoService.getIfaceMacAddress(ifaceName)
+        }
 
         model.detail = model.toString()
-    }
-
-    // Trata de obtener el nombre de la interfaz de red primaria
-    String getPrimaryIfaceName(){
-        SystemInfo si = new SystemInfo()
-        OperatingSystem os = si.getOperatingSystem()
-        HardwareAbstractionLayer hal = si.getHardware()
-
-        String gatewayIP = os.getNetworkParams().ipv4DefaultGateway
-        String iface
-        // Trato de encontrar una interfaz en la misma subred que el gateway
-        hal.getNetworkIFs().each { NetworkIF net ->
-            if(!iface){
-                net.IPv4addr.each {
-                    if(!iface){
-                        if(mismaRedClaseC(it, gatewayIP)){
-                            iface = net.name
-                        }
-                    }
-                }
-            }
-        }
-        // Si no encontré una placa con IP en la red del default Gateway
-        // tomo la primera placa de red con tráfico entrante
-        if(!iface){
-            hal.getNetworkIFs().any { NetworkIF net ->
-                if(net.getBytesRecv() > 0){
-                    iface = net.name
-                    true
-                } else {
-                    return
-                }
-            }
-        }
-        iface
-    }
-
-    // Setea los parámetros de hardware con la interfaz de red ifName
-    def setNetworkInfo(String ifName){
-        SystemInfo si = new SystemInfo()
-        HardwareAbstractionLayer hal = si.getHardware()
-        model.netIF = model.macAdress = ""
-        hal.getNetworkIFs().each { NetworkIF net ->
-            if(ifName == net.name){
-                model.netIF = net.name
-                model.macAdress = net.getMacaddr()
-            }
-        }
-    }
-
-    //Compara los tres primeros octetos de dos direcciones IP
-    Boolean mismaRedClaseC(String ip1, String ip2){
-        (ip1?.split('\\.')[0..2]?.join('.') == ip2?.split('\\.')[0..2]?.join('.'))
     }
 }
--- griffon-app/services/io/github/aretche/hwlist/HwInfoService.groovy
+++ griffon-app/services/io/github/aretche/hwlist/HwInfoService.groovy
@@ -1,6 +1,8 @@
 package io.github.aretche.hwlist
 
 import griffon.core.artifact.GriffonService
+import oshi.SystemInfo
+import oshi.hardware.NetworkIF
 
 /**
  * Created by arellanog on 27/10/17.
@@ -9,4 +11,97 @@ import griffon.core.artifact.GriffonService
 @griffon.metadata.ArtifactProviderFor(GriffonService)
 class HwInfoService {
 
+    // Retorna el nombre del Sistema Operativo
+    String getOsName(SystemInfo si = null){
+        if(!si)
+            si = new SystemInfo()
+        si.operatingSystem.family
+    }
+
+    // Retorna el nombre del Sistema Operativo
+    String getOsVersion(SystemInfo si = null){
+        if(!si)
+            si = new SystemInfo()
+        si.operatingSystem.version
+    }
+
+    // Retorna el modelo del procesador
+    String getProcessor(SystemInfo si = null){
+        if(!si)
+            si = new SystemInfo()
+        si.hardware.processor.identifier
+    }
+
+    // Retorna el ID del procesador
+    String getProcessorId(SystemInfo si = null){
+        if(!si)
+            si = new SystemInfo()
+        si.hardware.processor.processorID
+    }
+
+    // Retorna la marca del MotherBoard
+    String getbaseBoardMaker(SystemInfo si = null){
+        if(!si)
+            si = new SystemInfo()
+        si.hardware.computerSystem.baseboard.manufacturer
+    }
+
+    // Retorna el modelo del MotherBoard
+    String getbaseBoardModel(SystemInfo si = null){
+        if(!si)
+            si = new SystemInfo()
+        si.hardware.computerSystem.baseboard.model
+    }
+
+    // Retorna la MAC address de la interfaz con nombre ifName
+    String getIfaceMacAddress(String ifName, SystemInfo si = null){
+        String ifaceMacAddress
+        if(!si)
+            si = new SystemInfo()
+        si.hardware.networkIFs.each { NetworkIF net ->
+            if(ifName == net.name){
+               ifaceMacAddress = net.macaddr
+            }
+        }
+        ifaceMacAddress
+    }
+
+    // Trata de obtener el nombre de la interfaz de red primaria
+    String getPrimaryIfaceName(SystemInfo si = null){
+         if(!si)
+            si = new SystemInfo()
+
+        String gatewayIP = si.operatingSystem.networkParams.ipv4DefaultGateway
+        String iface
+        // Trato de encontrar una interfaz en la misma subred que el gateway
+        si.hardware.networkIFs.each { NetworkIF net ->
+            if(!iface){
+                net.IPv4addr.each {
+                    if(!iface){
+                        if(mismaRedClaseC(it, gatewayIP)){
+                            iface = net.name
+                        }
+                    }
+                }
+            }
+        }
+        // Si no encontré una placa con IP en la red del default Gateway
+        // tomo la primera placa de red con tráfico entrante
+        if(!iface){
+            si.hardware.networkIFs.any { NetworkIF net ->
+                if(net.getBytesRecv() > 0){
+                    iface = net.name
+                    true
+                } else {
+                    return
+                }
+            }
+        }
+        iface
+    }
+
+    //Compara los tres primeros octetos de dos direcciones IP
+    private Boolean mismaRedClaseC(String ip1, String ip2){
+        (ip1?.split('\\.')[0..2]?.join('.') == ip2?.split('\\.')[0..2]?.join('.'))
+    }
 }
