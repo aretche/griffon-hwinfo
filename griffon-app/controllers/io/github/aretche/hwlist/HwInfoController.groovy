91c0a6bd1ab4ac1ec1679ca186dc497964ed15b3
Gabriel Arellano <arellanog@cent.gov.ar>
2017-10-29 14:36:59 -0300

    Busco la interfaz de red primaria usando el default gateway

--- griffon-app/controllers/io/github/aretche/hwlist/HwInfoController.groovy
+++ griffon-app/controllers/io/github/aretche/hwlist/HwInfoController.groovy
@@ -32,28 +32,63 @@ class HwInfoController {
         model.cpuID = hal.processor.getProcessorID()
         model.cpu = hal.processor.getIdentifier()
 
-        // Tomo la primera placa de red con tráfico
-        hal.getNetworkIFs().any { NetworkIF net ->
-            if(net.getBytesRecv() > 0){
-                setNetworkInfo(net.name)
-                true
-            } else {
-                return
-            }
-        }
+        String ifaceName = getPrimaryIfaceName()
+        if(ifaceName)
+            setNetworkInfo(ifaceName)
 
         model.detail = model.toString()
     }
 
+    // Trata de obtener el nombre de la interfaz de red primaria
+    String getPrimaryIfaceName(){
+        SystemInfo si = new SystemInfo()
+        OperatingSystem os = si.getOperatingSystem()
+        HardwareAbstractionLayer hal = si.getHardware()
+
+        String gatewayIP = os.getNetworkParams().ipv4DefaultGateway
+        String iface
+        // Trato de encontrar una interfaz en la misma subred que el gateway
+        hal.getNetworkIFs().each { NetworkIF net ->
+            if(!iface){
+                net.IPv4addr.each {
+                    if(!iface){
+                        if(mismaRedClaseC(it, gatewayIP)){
+                            iface = net.name
+                        }
+                    }
+                }
+            }
+        }
+        // Si no encontré una placa con IP en la red del default Gateway
+        // tomo la primera placa de red con tráfico entrante
+        if(!iface){
+            hal.getNetworkIFs().any { NetworkIF net ->
+                if(net.getBytesRecv() > 0){
+                    iface = net.name
+                    true
+                } else {
+                    return
+                }
+            }
+        }
+        iface
+    }
+
+    // Setea los parámetros de hardware con la interfaz de red ifName
     def setNetworkInfo(String ifName){
         SystemInfo si = new SystemInfo()
         HardwareAbstractionLayer hal = si.getHardware()
         model.netIF = model.macAdress = ""
-        hal.getNetworkIFs().any { NetworkIF net ->
+        hal.getNetworkIFs().each { NetworkIF net ->
             if(ifName == net.name){
                 model.netIF = net.name
                 model.macAdress = net.getMacaddr()
             }
         }
+    }
+
+    //Compara los tres primeros octetos de dos direcciones IP
+    Boolean mismaRedClaseC(String ip1, String ip2){
+        (ip1?.split('\\.')[0..2]?.join('.') == ip2?.split('\\.')[0..2]?.join('.'))
     }
 }
