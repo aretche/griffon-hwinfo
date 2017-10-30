9dc3ec88164e89e08df3ae7b330583382bc89ee1
Gabriel Arellano <arellanog@cent.gov.ar>
2017-10-30 07:34:56 -0300

    Subo el logLevel a INFO y seteo los stacktrace en la salida de la aplicacion

--- src/main/groovy/io/github/aretche/hwlist/Launcher.groovy
+++ src/main/groovy/io/github/aretche/hwlist/Launcher.groovy
@@ -4,6 +4,9 @@ import griffon.javafx.JavaFXGriffonApplication
 
 public class Launcher {
     public static void main(String[] args) throws Exception {
+        System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "info")
+        System.setProperty("griffon.full.stacktrace", "true")
+        System.setProperty("griffon.exception.output","true")
         JavaFXGriffonApplication.main(args)
     }
 }
